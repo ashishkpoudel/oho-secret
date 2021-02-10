8d04f4e16779f4f111b82046a07d0901bbd5daee
Ashish <routeasis@gmail.com>
2021-02-10 21:10:24 +0545

    eslint

---
+++ api/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: { }
+};
