6d22fd57842b29a335e040b37ac41d0d047bac22
Ashish <routeasis@gmail.com>
2021-02-11 23:39:01 +0545

    add exception

--- api/src/secret/infrastructure/knex/repository/knex-get-secret.repository.ts
+++ api/src/secret/infrastructure/knex/repository/knex-get-secret.repository.ts
@@ -1,10 +1,16 @@
 import { db } from 'database';
 import { GetSecretRepository } from 'secret/application/get-secret/get-secret.repository';
 import { Secret } from 'secret/application/get-secret/secret';
+import { SecretNotFound } from 'secret/domain/exception/secret-not-found';
 
 export class KnexGetSecretRepository implements GetSecretRepository {
   async byId(id: string): Promise<Secret> {
     const secret = await db('secrets').where({ id }).first();
+
+    if (!secret) {
+      throw new SecretNotFound('Secret not found');
+    }
+
     return new Secret({
       id: secret.id,
       body: secret.body,
--- api/src/secret/infrastructure/knex/repository/knex-secret.repository.ts
+++ api/src/secret/infrastructure/knex/repository/knex-secret.repository.ts
@@ -2,10 +2,16 @@ import { db } from 'database';
 import { SecretRepository } from 'secret/domain/repository/secret.repository';
 import { Secret } from 'secret/domain/model/secret';
 import { SecretMapper } from 'secret/infrastructure/mapper/secret-mapper';
+import { SecretNotFound } from 'secret/domain/exception/secret-not-found';
 
 export class KnexSecretRepository implements SecretRepository {
   async getById(id: string): Promise<Secret> {
     const secret = await db('secrets').where({ id }).first();
+
+    if (!secret) {
+      throw new SecretNotFound('Secret not found');
+    }
+
     return SecretMapper.toDomain(secret);
   }
 
