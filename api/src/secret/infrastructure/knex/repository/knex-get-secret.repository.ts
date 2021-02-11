46b67afe50a92746941016e935338d14a286e6ed
Ashish <routeasis@gmail.com>
2021-02-11 22:58:39 +0545

    use knex replacing typeorm

--- api/src/secret/infrastructure/knex/repository/knex-get-secret.repository.ts
+++ api/src/secret/infrastructure/knex/repository/knex-get-secret.repository.ts
@@ -1,13 +1,10 @@
-import { getRepository } from 'typeorm';
-import { TypeormSecretEntity } from 'secret/infrastructure/typeorm/entity/typeorm-secret.entity';
+import { db } from 'database';
 import { GetSecretRepository } from 'secret/application/get-secret/get-secret.repository';
 import { Secret } from 'secret/application/get-secret/secret';
 
-export class TypeormGetSecretRepository implements GetSecretRepository {
+export class KnexGetSecretRepository implements GetSecretRepository {
   async byId(id: string): Promise<Secret> {
-    const repository = getRepository(TypeormSecretEntity);
-    const secret = await repository.findOneOrFail({ id });
-
+    const secret = await db('secrets').where({ id }).first();
     return new Secret({
       id: secret.id,
       body: secret.body,
--- api/src/secret/infrastructure/knex/repository/knex-secret.repository.ts
+++ api/src/secret/infrastructure/knex/repository/knex-secret.repository.ts
@@ -1,17 +1,16 @@
-import { getRepository } from 'typeorm';
+import { db } from 'database';
 import { SecretRepository } from 'secret/domain/repository/secret.repository';
 import { Secret } from 'secret/domain/model/secret';
 import { SecretMapper } from 'secret/infrastructure/mapper/secret-mapper';
-import { TypeormSecretEntity } from 'secret/infrastructure/typeorm/entity/typeorm-secret.entity';
 
-export class TypeormSecretRepository implements SecretRepository {
+export class KnexSecretRepository implements SecretRepository {
   async getById(id: string): Promise<Secret> {
-    const secret = await getRepository(TypeormSecretEntity).findOneOrFail({ id });
+    const secret = await db('secrets').where({ id }).first();
     return SecretMapper.toDomain(secret);
   }
 
   async save(secret: Secret): Promise<void> {
     const data = SecretMapper.toPersistence(secret);
-    await getRepository(TypeormSecretEntity).save(data);
+    await db('secrets').insert(data);
   }
 }
